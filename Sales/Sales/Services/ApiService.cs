544142434813ffc8b85585cbf594a99e42bd8b94
John Alexander Jimenez Morales <jhonalexjm@gmail.com>
2018-08-21 19:37:12 -0500

    create service api generic

--- Sales/Sales/Services/ApiService.cs
+++ Sales/Sales/Services/ApiService.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Sales.Common.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Text;
+
+/// <summary>
+/// sirve para consumir cualquie lista de objetos es generica
+/// 
+/// </summary>
 
 namespace Sales.Services
 {
     class ApiService
     {
+        public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = $"{prefix}{controller}";
+                var response = await client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+
+                if(response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = answer,
+
+                    };
+                }
+
+
+                var list = JsonConvert.DeserializeObject<List<T>>(answer);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = list,
+
+                };
+
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+
+                throw;
+            }
+        }
     }
 }
