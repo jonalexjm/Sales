de41482da0161e926cfa3d14f04653176b9189c1
John Alexander Jimenez Morales <jhonalexjm@gmail.com>
2018-08-31 11:45:24 -0500

    automatic migration
     <connectionStrings> is inactive

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -2,7 +2,7 @@
 
 namespace Sales.Common.Models
 
-{
+{// se tiene que habilitar las migraciones automaticas
 
     using System;
     using System.Collections.Generic;
@@ -17,11 +17,27 @@ namespace Sales.Common.Models
         public int ProductId { get; set; }
 
         [Required]
+        [StringLength(50)]//only you can to write 50 characters
         public string Description { get; set; }
+
+        [DataType(DataType.MultilineText)]
+
+        public string Remarks { get; set; }
+
+        [Display (Name = "Image")]
+
+        public string ImagePath { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
 
         public Decimal Price { get; set; }
 
+        [Display(Name = "Is Available")]
+
         public Boolean IsAvailable { get; set; }
+
+        [Display(Name = "Publish On")]
+        [DataType(DataType.Date)]
 
         public DateTime PublishOn { get; set; }
 
@@ -29,5 +45,7 @@ namespace Sales.Common.Models
         {
             return this.Description;
         }
+
+
     }
 }
