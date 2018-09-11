dd7eb8fef558696fa35fa41ddd225e0cff760a9b
John Alexander Jimenez Morales <jhonalexjm@gmail.com>
2018-09-11 17:03:16 -0500

    search bar is ready

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -17,6 +17,9 @@ namespace Sales.ViewModels
     {
 
         #region Attributes
+
+        private string filter;
+
         private ApiService apiService;
 
         private bool isRefreshing;
@@ -30,6 +33,18 @@ namespace Sales.ViewModels
 
         #region Properties
 
+        public string Filter
+        {
+            get { return this.filter; }
+
+            set
+            {
+                this.filter = value;
+                this.RefreshList();
+            }
+
+
+        }
 
         public ObservableCollection<ProductItemViewModel> Products
         {
@@ -136,24 +151,62 @@ namespace Sales.ViewModels
 
         public void RefreshList()
         {
-            var myListProductItemViewModel = MyProducts.Select(p => new ProductItemViewModel
+            if(string.IsNullOrEmpty(this.Filter))
             {
-                Description = p.Description,
-                ImageArray = p.ImageArray,
-                ImagePath = p.ImagePath,
-                IsAvailable = p.IsAvailable,
-                Price = p.Price,
-                ProductId = p.ProductId,
-                PublishOn = p.PublishOn,
-                Remarks = p.Remarks,
-            });
+                var myListProductItemViewModel = MyProducts.Select(p => new ProductItemViewModel
+                {
+                    Description = p.Description,
+                    ImageArray = p.ImageArray,
+                    ImagePath = p.ImagePath,
+                    IsAvailable = p.IsAvailable,
+                    Price = p.Price,
+                    ProductId = p.ProductId,
+                    PublishOn = p.PublishOn,
+                    Remarks = p.Remarks,
+                });
 
-            this.Products = new ObservableCollection<ProductItemViewModel>(myListProductItemViewModel.OrderBy(p => p.Description));
+                this.Products = new ObservableCollection<ProductItemViewModel>(
+                    myListProductItemViewModel.OrderBy(p => p.Description));
+
+            }
+            else
+            {
+                var myListProductItemViewModel = this.MyProducts.Select(p => new ProductItemViewModel
+                {
+                    Description = p.Description,
+                    ImageArray = p.ImageArray,
+                    ImagePath = p.ImagePath,
+                    IsAvailable = p.IsAvailable,
+                    Price = p.Price,
+                    ProductId = p.ProductId,
+                    PublishOn = p.PublishOn,
+                    Remarks = p.Remarks,
+                }).Where(p => p.Description.ToLower().Contains(this.Filter.ToLower())).ToList();
+
+                this.Products = new ObservableCollection<ProductItemViewModel>(
+                    myListProductItemViewModel.OrderBy(p => p.Description));
+
+            }
+           
+
+            
 
         }
         #endregion
 
         #region Commands
+
+        public ICommand SearchCommand
+        {
+
+            get
+            {
+                return new RelayCommand(RefreshList);
+            }
+
+        }
+
+
         public ICommand RefreshCommand
         {
 
